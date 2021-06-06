283b21c972d3838496a6b8154de3dd18a7a97172
ekta2208 <51887162+ekta2208@users.noreply.github.com>
2021-06-06 11:48:38 +0530

    Create week-1.py

---
+++ week-1.py
@@ -0,0 +1,10 @@
+# This Python notebook is created for submission of the first Assignment of Applied Data Science Capstone. In this notebook I will import pandas library and numpy library and then push it to my github repository.
+
+
+
+import pandas as pd
+import numpy as np
+
+
+
+print("Hello Capstone Project Course!")
